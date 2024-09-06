3d663d3fa3494632afb52a2530b3b9345a90879f
AyushGupta11442 <ayushgupta11442@gmail.com>
2024-09-06 12:45:42 +0530

    learning going on

---
+++ dom/index.js
@@ -0,0 +1,41 @@
+// getElementbyId
+
+// const title = document.getElementById('main-heading');
+// console.log(title);
+// console.log(title.innerHTML);
+
+
+
+// // getElementByClassName
+
+
+// const ListItems = document.getElementsByClassName('list-items');
+// console.log(ListItems);
+
+
+
+// // getElementByTagName
+
+// const listItem = document.getElementsByTagName('li');
+// console.log(listItem)
+
+
+// // querrySelector
+
+// const container = document.querySelector('div');
+// console.log(container);
+
+// querySelectorAll
+
+// const container = document.querySelectorAll('div');
+// console.log(container);
+
+
+
+
+// styling the elements
+
+const title = document.querySelector('#main-heading');
+
+title.style.color = 'red';
+console.log(title.style);
